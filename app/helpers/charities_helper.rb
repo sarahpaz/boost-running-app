b3e288aa09b1f4fb77a06489b2dbaa5bb8ba4a09
sarahpaz <sarah.paz@gmail.com>
2019-07-31 00:22:17 -0400

    create runs_to_view helper method

--- app/helpers/charities_helper.rb
+++ app/helpers/charities_helper.rb
@@ -1,3 +1,16 @@
 module CharitiesHelper
-  
+  def top_runner
+    if @charity.runs.present?
+      User.total_charity_runs(@charity)
+    else
+      tag.p("No runs yet!")
+    end
+  end
+
+  def runs_to_view
+    if @charity.runs.present?
+      link_to("View Runs", charity_runs_path(@charity), :class => 'boost-btn btn-primary btn-sm').html_safe
+    end
+  end
 end
+
