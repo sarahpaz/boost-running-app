bb1298467ac30d003dd7befea108c74e9173fa95
Sarah Paz <sarah.paz@gmail.com>
2019-09-20 01:28:36 -0400

    add constructor and prototype functions

--- app/assets/javascripts/charities.js
+++ app/assets/javascripts/charities.js
@@ -1,3 +1,4 @@
+//****** Document ready
 $(() => {
   charityClickHandler();
 });
@@ -5,6 +6,37 @@ $(() => {
 const charityClickHandler = () => {
   $(".charities").on("click", e => {
     e.preventDefault();
-    alert("it works");
+    // history.pushState(null, null, "/charities");
+    fetch("/charities.json")
+      .then(res => res.json())
+      .then(charities => {
+        $("#charities-container").html("");
+        charities.forEach(charity => {
+          let newCharity = new Charity(charity);
+          let charityHtml = newCharity.formatIndex();
+          $("#charities-container").append(charityHtml);
+        });
+      });
   });
+};
+
+//****** Constructor functions
+class Charity {
+  constructor(charity) {
+    this.name = charity.name;
+    this.description = charity.description;
+    this.id = charity.id;
+    this.runs = charity.runs;
+  }
+}
+
+//****** Prototype functions
+Charity.prototype.formatIndex = function() {
+  let charityHtml = `
+		<ul>
+			<li>${this.name}</li>
+			<p>${this.description}</p>
+		</ul>
+	`;
+  return charityHtml;
 };
