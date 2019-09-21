7d9c6729a5515f316879f1df0cf404958108ecfb
Sarah Paz <sarah.paz@gmail.com>
2019-09-21 10:36:00 -0400

    change sprocket order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,9 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-// require_tree
 //= require jquery
-//= require popper
+//= require jquery_ujs
 //= require bootstrap-sprockets
+
 //= require charities
+//= require_tree .
