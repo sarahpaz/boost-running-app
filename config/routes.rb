b5d395453b21968f2db9141e2828afa11a2b0759
Sarah Paz <sarah.paz@gmail.com>
2019-09-22 15:50:05 -0400

    add new charity route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :runs
-  resources :charities, only: [:index, :show]
+  resources :charities, only: [:index, :show, :new]
   resources :users, only: [:index, :new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'sessions#index'
