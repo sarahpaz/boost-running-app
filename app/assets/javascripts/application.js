6998dfcfc69daedfacf94eb0fcb3bb8401beb8b7
Sarah Paz <sarah.paz@gmail.com>
2019-09-23 20:37:00 -0400

    remove turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
+
 //= require jquery
-//= require jquery_ujs
+//= require rails-ujs
+//= require turbolinks
 //= require bootstrap-sprockets
+//= require activestorage
 
 //= require charities
-//= require_tree .
