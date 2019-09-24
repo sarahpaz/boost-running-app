34064b27834170075d31cf8aa69ee5b8a028d13c
Sarah Paz <sarah.paz@gmail.com>
2019-09-23 21:26:07 -0400

    move rails-ujs to be first in list

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 
-//= require jquery
 //= require rails-ujs
+//= require jquery
+//= require turbolinks
 //= require bootstrap-sprockets
 //= require activestorage
 
